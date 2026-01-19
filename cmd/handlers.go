984b3ba4d91c7ed738769be8a7945969d72ac51b
Danilo Cordeiro <dev.danilocordeiro@gmail.com>
2026-01-18 23:24:58 -0300

    add notifications and events

--- cmd/handlers.go
+++ cmd/handlers.go
@@ -3,13 +3,15 @@ package main
 import "github.com/dev-danilocordeiro/pizza-shop-order/internal/models"
 
 type Handler struct {
-	orders *models.OrderModel
-	users  *models.UserModel
+	orders              *models.OrderModel
+	users               *models.UserModel
+	notificationManager *NotificationManager
 }
 
 func NewHandler(dbModel *models.DBModel) *Handler {
 	return &Handler{
-		orders: &dbModel.Order,
-		users:  &dbModel.User,
+		orders:              &dbModel.Order,
+		users:               &dbModel.User,
+		notificationManager: NewNotificationManager(),
 	}
 }
